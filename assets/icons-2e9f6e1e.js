3fe801417fa3490fdf95c5140b73e58ac88a009f
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-26 14:45:02 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-11@df3ed956fe313e27f71045e227f102b9b8d5261c 🚀

--- assets/icons-2e9f6e1e.js
+++
@@ -1,2 +0,0 @@
-const s="/goit-js-homework-11/assets/icons-f810c139.svg";export{s as i};
-//# sourceMappingURL=icons-2e9f6e1e.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,9 +1,9 @@
-import"./assets/create-cards-gallery-6a214f9d.js";/* empty css                      */import{S as i}from"./assets/vendor-9310f15c.js";import{i as s}from"./assets/insert-cards-to-gallery-a80f76a8.js";import{i as e}from"./assets/icons-2e9f6e1e.js";const a=document.querySelector(".gallery"),n=document.querySelector(".loader");let l=new i(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
-          <use href="${e}#icon-chevron-left"></use>
+import{i as c,c as l,a as i}from"./assets/create-msg-e6f4ee7d.js";/* empty css                      */import{S as h}from"./assets/vendor-9310f15c.js";const o="/goit-js-homework-11/assets/icons-f810c139.svg",a=document.querySelector(".gallery"),t=document.querySelector(".loader"),s=document.querySelector(".search-form");s.addEventListener("submit",u);let n=new h(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
+          <use href="${o}#icon-chevron-left"></use>
         </svg>`,`<svg class="lightbox-button-icon-next" width="24" height="24" aria-label="icon arrow right">
-          <use href="${e}#icon-chevron-right"></use>
+          <use href="${o}#icon-chevron-right"></use>
         </svg>`],close:!0,closeText:`
         <svg class="lightbox-close-icon" width="16" height="16">
-          <use href="${e}#close-form"></use>
-        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});const t=document.querySelector(".search-form");t.addEventListener("submit",c);function c(o){o.preventDefault(),o.currentTarget;try{s({searchForm:t,galleryRef:a,modalWndRef:l,loaderRef:n})}catch(r){console.error("Error:",r)}}
+          <use href="${o}#close-form"></use>
+        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function u(e){e.preventDefault();try{t.style.display="block";const r=e.currentTarget.search.value.trim();c(r,f,g)}catch(r){console.error("Error:",r)}}function g(e){s.search.value="",t.style.display="none",l(e),a.innerHTML="",n.refresh()}function f(e){s.search.value="",t.style.display="none",i(e,a),n.refresh()}
 //# sourceMappingURL=commonHelpers2.js.map
