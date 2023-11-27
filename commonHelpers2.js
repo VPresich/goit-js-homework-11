d254f10e1ec18abf874d386b9f75c4df0b29d987
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-27 19:31:32 +0000

    Deploying to gh-pages from @ VPresich/goit-js-homework-11@9cc5ebe865ac0366ca94bbbf181292d726f9bc9f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,9 +1,9 @@
-import{g as l,c as i,B as h,a as u,b as g}from"./assets/create-msg-8409e0f8.js";/* empty css                      */import{S as m}from"./assets/vendor-9310f15c.js";const a="/goit-js-homework-11/assets/icons-f810c139.svg",r=document.querySelector(".gallery"),o=document.querySelector(".loader"),n=document.querySelector(".search-form");n.addEventListener("submit",d);let c=new m(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
-          <use href="${a}#icon-chevron-left"></use>
+import{g as l,c as i,B as h,a as u,b as g}from"./assets/create-msg-072109d4.js";/* empty css                      */import{S as m}from"./assets/vendor-9310f15c.js";const t="/goit-js-homework-11/assets/icons-f810c139.svg",o=document.querySelector(".gallery"),s=document.querySelector(".loader"),a=document.querySelector(".search-form");a.addEventListener("submit",f);let n=new m(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
+          <use href="${t}#icon-chevron-left"></use>
         </svg>`,`<svg class="lightbox-button-icon-next" width="24" height="24" aria-label="icon arrow right">
-          <use href="${a}#icon-chevron-right"></use>
+          <use href="${t}#icon-chevron-right"></use>
         </svg>`],close:!0,closeText:`
         <svg class="lightbox-close-icon" width="16" height="16">
-          <use href="${a}#close-form"></use>
-        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function d(e){e.preventDefault();try{o.style.display="block";const t=e.currentTarget.search.value.trim();l(t).then(s=>y(s)).catch(s=>f(s))}catch(t){console.error("Unknown Error:",t),o.style.display="none"}}function f(e){n.search.value="",o.style.display="none",i(e),r.style.backgroundColor=h,r.innerHTML="",c.refresh()}function y(e){n.search.value="",o.style.display="none",r.style.backgroundColor=u,g(e,r),c.refresh()}
+          <use href="${t}#close-form"></use>
+        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function f(e){e.preventDefault(),s.style.display="block";const c=e.currentTarget.search.value.trim();l(c).then(r=>b(r)).catch(r=>d(r))}function d(e){a.search.value="",s.style.display="none",i(e),o.style.backgroundColor=h,o.innerHTML="",n.refresh()}function b(e){a.search.value="",s.style.display="none",o.style.backgroundColor=u,g(e,o),n.refresh()}
 //# sourceMappingURL=commonHelpers2.js.map
